881881d5f2f63b6b909f094e9cb4103ca8189d24
Kartik Boodi <kboodi@deloitte.com>
2022-05-12 13:26:19 +0530

    removed comments

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { compose } from "redux";
 import authenticationreducer from "./Authentication/reducer";
 import CartReducer from "./Cart/CartSlice";
-
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
 
 export const store = configureStore({
   reducer: {
