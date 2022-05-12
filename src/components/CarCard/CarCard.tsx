9a990851f9461df805de4a80eade831c54730541
Atrey <vatrey@deloitte.com>
2022-05-12 15:47:43 +0530

    Added input field in Car Listing page for search

--- src/components/CarCard/CarCard.tsx
+++ src/components/CarCard/CarCard.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./CarCard.scss";
 import rightarrow from "../../assets/rightArrow.svg";
-import { IcarData } from "../../models/ICarData";
+import { CarDetails } from "../../models/CarDetails";
 
-const CarCard = (carData: IcarData) => {
+const CarCard = (props: {carData: CarDetails}) => {
   return (
     <div className="car-card-main">
-      <img className="car-card-car-image" src={carData.img} alt="car-img" />
+      <img className="car-card-car-image" src={props.carData.image} alt="car-img" />
       <div className="car-card-data">
         <div>
-          <h6> {carData.name}</h6>
-          <p>{carData.price} kh onwards</p>
+          <h6> {props.carData.name}</h6>
+          <p>{props.carData.price} kh onwards</p>
         </div>
         <div>
           <img src={rightarrow} alt="detail" />
