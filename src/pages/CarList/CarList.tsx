6c7d15bacb6b92055f4deeb06d8980615318a58a
Kartik Boodi <kboodi@deloitte.com>
2022-05-12 10:19:46 +0530

    reverted changes in carlist

--- src/pages/CarList/CarList.tsx
+++ src/pages/CarList/CarList.tsx
@@ -1,39 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import CarCard from "../../components/CarCard/CarCard";
-import { IcarData } from "../../models/ICarData";
-import { RootState } from "../../redux/store";
-
-const carsList = [
-  {
-    id: "hondacity",
-    name: "Honda City",
-    img: "",
-    price: "12 L",
-  },
-  {
-    id: "swift",
-    name: "Maruti Swift",
-    img: "",
-    price: "12 L",
-  },
-  {
-    id: "altroz",
-    name: "Tata Altroz",
-    img: "",
-    price: "12 L",
-  },
-];
 
 const CarList = () => {
-  console.log(useSelector((state: RootState) => state.cart.items));
-  return (
-    <div>
-      {carsList.map((car: IcarData, index) => (
-        <CarCard key={index} carData={car} />
-      ))}
-    </div>
-  );
+  return <div>CarList</div>;
 };
 
 export default CarList;
