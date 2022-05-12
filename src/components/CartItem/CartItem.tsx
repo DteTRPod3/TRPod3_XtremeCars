18519860624c354033d35e3f3c8329823031ffb5
Kartik Boodi <kboodi@deloitte.com>
2022-05-12 15:07:37 +0530

    removed nagation operator from atributes passed to dispatch actions

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -17,12 +17,12 @@ function CartItem(cardData: any) {
       <div className="cart-item-image">car image</div>
       <p>{car?.name}</p>
       <div className="cart-item-quantity-div">
-        <button onClick={() => dispatch(decreaseQuantity(car!))}>-</button>
-        <span>Qty: {quantity!}</span>
-        <button onClick={() => dispatch(increaseQuantity(car!))}>+</button>
+        <button onClick={() => dispatch(decreaseQuantity(car))}>-</button>
+        <span>Qty: {quantity}</span>
+        <button onClick={() => dispatch(increaseQuantity(car))}>+</button>
       </div>
       <p>{car?.price}</p>
-      <button onClick={() => dispatch(removeFromCart(car!))}>
+      <button onClick={() => dispatch(removeFromCart(car))}>
         Remove from Cart
       </button>
     </div>
